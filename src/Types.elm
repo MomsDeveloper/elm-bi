fe1e5bb2f679e3e2e5d8bd6319d059a5e5155fe4
Георгий Хабнер <georgiy.khabner@mail.ru>
2024-12-24 20:34:59 +0300

    start project

---
+++ src/Types.elm
@@ -0,0 +1,36 @@
+module Types exposing (..)
+
+import Browser exposing (UrlRequest)
+import Browser.Navigation exposing (Key)
+import Url exposing (Url)
+
+
+type alias FrontendModel =
+    { key : Key
+    , message : String
+    }
+
+
+type alias BackendModel =
+    { message : String
+        -- files : List String
+        -- dashboard : List String
+    }
+
+
+type FrontendMsg
+    = UrlClicked UrlRequest
+    | UrlChanged Url
+    | NoOpFrontendMsg
+
+
+type ToBackend
+    = NoOpToBackend
+
+
+type BackendMsg
+    = NoOpBackendMsg
+
+
+type ToFrontend
+    = NoOpToFrontend
